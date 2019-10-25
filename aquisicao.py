34f289e6b995a3087079881a472462c35a15b93b
William Felipe <theorangewill@gmail.com>
2019-10-24 23:07:22 -0300

    Aquisicao de dados

--- aquisicao.py
+++ aquisicao.py
@@ -3,7 +3,19 @@
 import xml.etree.ElementTree as ET
 import requests
 
+arq = open('dados.txt', 'w')
+
 deputados = [204534, 204464, 160672, 92346, 160976]
+periodos = [["2015-01-01", "2015-06-30"],
+            ["2015-07-01", "2015-12-31"],
+            ["2016-01-01", "2016-06-30"],
+            ["2016-07-01", "2016-12-31"],
+            ["2017-01-01", "2017-06-30"],
+            ["2017-07-01", "2017-12-31"],
+            ["2018-01-01", "2018-06-30"],
+            ["2018-07-01", "2018-12-31"],
+            ["2019-01-01", "2019-06-30"],
+            ["2019-07-01", "2019-12-31"],]
 
 for deputado in deputados:
     url = "https://dadosabertos.camara.leg.br/api/v2/deputados/" + str(deputado)
@@ -34,24 +46,31 @@ for deputado in deputados:
             #print(siglaPartido.tag, siglaPartido.text)
             perfil.append(siglaPartido.text)
 
-    print perfil
-    continue
-    url = "https://dadosabertos.camara.leg.br/api/v2/deputados/92346/discursos?dataInicio=2015-01-01&dataFim=2015-12-31&ordenarPor=dataHoraInicio&ordem=ASC"
-    header = { 'Accept': 'application/xml' }
-    r = requests.get(url, headers=header)
+    #print perfil
+    for periodo in periodos:
+        url = "https://dadosabertos.camara.leg.br/api/v2/deputados/"+str(deputado)+"/discursos?dataInicio="+str(periodo[0])+"&dataFim="+str(periodo[1])+"&ordenarPor=dataHoraInicio&ordem=ASC"
+        header = { 'Accept': 'application/xml' }
+        r = requests.get(url, headers=header)
 
-    tree =  ET.ElementTree(ET.fromstring(r.content))
+        tree =  ET.ElementTree(ET.fromstring(r.content))
 
-    #arquivo = "discursos?dataInicio=2015-01-01.xml"
-    #tree =  ET.parse(arquivo)
+        #arquivo = "discursos?dataInicio=2015-01-01.xml"
+        #tree =  ET.parse(arquivo)
 
-    root = tree.getroot()
+        root = tree.getroot()
 
-    filtro = "*"
-    for child in root.iter(filtro):
-        #print(child.tag, child.text)
-        for sumario in child.findall("tipoDiscurso"):
-            print(sumario.text)
-        for discurso in child.findall("transcricao"):
-            print(discurso.text)
-            print("\n")
+        filtro = "*"
+        discurso = []
+        for child in root.iter(filtro):
+            #print(child.tag, child.text)
+            for tipoDiscurso in child.findall("tipoDiscurso"):
+                #print(tipoDiscurso.text)
+                discurso.append(tipoDiscurso.text)
+            for transcricao in child.findall("transcricao"):
+                discurso.append(transcricao.text)
+                #print(discurso.text)
+                #print("\n")
+        if len(discurso) == 2:
+            arq.write(perfil[0]+";"+perfil[1]+";"+perfil[2]+";"+perfil[3]+";"+perfil[4]+";"+perfil[5]+";"+discurso[0]+";"+discurso[1])
+        print perfil,discurso
+arq.close()
